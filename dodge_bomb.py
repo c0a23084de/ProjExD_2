a61e888e5cedccb150b3df23a08f9491088b6130
power suzuki <c0a23084de@edu.teu.ac.jp>
2024-06-18 14:27:20 +0900

    練習2 爆弾追加 修正

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -21,9 +21,9 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
-    bb_img = pg.Surface(20, 20)
+    bb_img = pg.Surface((20, 20))  # 練習2
     bb_img.set_colorkey((0, 0, 0))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10, 10))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     vx, vy = +5, +5
@@ -44,7 +44,7 @@ def main():
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)
+        bb_rct.move_ip(vx, vy)  # 練習2
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
