45c4982556a444ac0f575b22002fd9a8035b98db
power suzuki <c0a23084de@edu.teu.ac.jp>
2024-06-18 17:05:28 +0900

    演習3 ゲームオーバー演出

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import os
 import random
 import sys
 import pygame as pg
+import time
 
 
 WIDTH, HEIGHT = 1400, 800
@@ -11,14 +12,27 @@ DELTA = {  # 移動量辞書
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0),
 }
+def kk_angle():
+    pg.transform.flip(pg.image.load("fig/3.png"), 0, 2.0),
+    return  {
+        (-5, 0):pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0),
+        (-5, +5):pg.transform.rotozoom(pg.image.load("fig/3.png"), 45, 2.0),
+        (0, +5):pg.transform.rotozoom(pg.image.load("fig/3.png"), 90, 2.0),
+        (+5, +5):pg.transform.rotozoom(pg.image.load("fig/3.png"), 135, 2.0),
+        (+5, 0):pg.transform.rotozoom(pg.image.load("fig/3.png"), 180, 2.0),
+        (+5, -5):pg.transform.rotozoom(pg.image.load("fig/3.png"), 225, 2.0),
+        (0, -5):pg.transform.rotozoom(pg.image.load("fig/3.png"), 270, 2.0),
+        (-5, -5):pg.transform.rotozoom(pg.image.load("fig/3.png"), 315, 2.0),
+    }
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """
-    引数：こうかとんRect，または，爆弾Rect
+    引数:こうかとんRect,または爆弾Rect
     戻り値：真理値タプル（横方向，縦方向）
-    画面内ならTrue／画面外ならFalse
+    画面内ならTrue,画面外ならFalse
     """
     yoko, tate = True, True
     if rct.left < 0 or WIDTH < rct.right:  # 横方向判定
@@ -32,7 +46,7 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+    kk_img = pg.transform. rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     bb_img = pg.Surface((20, 20))  # 1辺が20の空のSurfaceを作る
@@ -48,7 +62,7 @@ def main():
             if event.type == pg.QUIT: 
                 return
         if kk_rct.colliderect(bb_rct):  # 衝突判定
-            return  # ゲームオーバー
+            return gameOver(screen)  # ゲームオーバー
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -73,6 +87,33 @@ def main():
         tmr += 1
         clock.tick(50)
 
+def gameOver(screen):
+    """
+    こうかとんが爆弾に衝突した際に
+    ・画面をブラックアウトする
+    ・泣いているこうかとんと
+    ・「Game Over」の文字列を表示する
+    ・表示時間は５秒間
+    """
+
+    black_img = pg.Surface((WIDTH,HEIGHT))
+    pg.draw.rect(black_img,(0,0,0), pg.Rect(0,0,WIDTH, HEIGHT))
+    black_img.set_alpha(200)
+    black_rct = black_img.get_rect()
+    screen.blit(black_img,black_rct)
+    fonto = pg.font.Font(None,80)
+    txt = fonto.render("Game Over",True,(255, 255, 255))
+    txt_rct = txt.get_rect()
+    txt_rct.center=WIDTH/2, HEIGHT/2
+    screen.blit(txt,txt_rct)
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = WIDTH/2-250, HEIGHT/2
+    screen.blit(kk_img,kk_rct)
+    kk_rct.center = WIDTH/2+250, HEIGHT/2
+    screen.blit(kk_img,kk_rct)
+    pg.display.update()
+    time.sleep(5)
 
 if __name__ == "__main__":
     pg.init()
